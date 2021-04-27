28cdc4d71f5fbeed41fd8c4fc7dd9a69dc09baf8
modit jain <moditjain121@gmail.com>
2021-04-27 15:12:37 +0200

    Build: Fixup! KangOS Sepolicy

--- core/config.mk
+++ core/config.mk
@@ -1245,6 +1245,13 @@ dont_bother_goals := out \
     vbmetaimage-nodeps \
     product-graph dump-products
 
+ifneq ($(KANGOS_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/kangos/sepolicy/common/sepolicy.mk)
+endif
+
+
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
@@ -1254,12 +1261,6 @@ endif
 -include external/ltp/android/ltp_package_list.mk
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
-
-ifneq ($(KANGOS_BUILD),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/kangos/sepolicy/common/sepolicy.mk)
-endif
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
